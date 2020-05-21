fb86050467797e64b9680bb464cc36abe655d691
Taku Nakai <tnakaicode@gmail.com>
2020-05-21 19:25:37 +0900

    plot pnt

---
+++ src/plot_pnt.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import time
+from optparse import OptionParser
+
+sys.path.append(os.path.join("../"))
+from src.base import plot2d, plotocc
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = OptionParser()
+    parser.add_option("--dir", dest="dir", default="./")
+    opt, argc = parser.parse_args(argvs)
+    print(opt, argc)
+
+    obj = plotocc()
+    obj.show_ellipsoid()
+    obj.show_pnt()
+    obj.show()
